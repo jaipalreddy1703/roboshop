7c56a7d50ff95289f8ad459d5bf19cfb5c19ccc5
Jai <jaipalreddy1703@gmail.com>
2026-02-02 04:36:50 +0530

    roboshop.sh

---
+++ roboshop.sh
@@ -0,0 +1,15 @@
+!#/bin/bash
+
+SG_ID="sg-005f466126c3865b6"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+    aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications  "ResourceType=instance,Tags[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].PrivateIpAddress' \
+    --output text
+done
