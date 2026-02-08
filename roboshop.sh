96c6913b169ad00f5e0eb1af5c715e7791c595ce
Jai <jaipalreddy1703@gmail.com>
2026-02-08 08:32:08 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -9,63 +9,58 @@ ZONE_ID="Z00087883QCEFVVFLOJWL"
 
 
 for instance in $@
-do 
-    # ===== CREATE INSTANCE =====
-    INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id "$AMI_ID" \
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
     --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --security-group-ids "$SG_ID" \
     --query 'Instances[0].InstanceId' \
-    --output text)
+    --output text )
 
-    if [ $? -ne 0 ]; then
-        echo "Failed to launch instance $instance."
-        exit 1  
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
     fi
 
-    echo "$instance created: $INSTANCE_ID"
+    echo "IP Address: $IP"
 
-    # ===== WAIT UNTIL RUNNING =====
-    aws ec2 wait instance-running \
-    --instance-ids "$INSTANCE_ID" \
-    --region "us-east-1"
-    echo "Instance of $instance is running.."
-
-    # ===== GET PUBLIC IP =====
-    if [ $instance == "frontend" ]; then 
-        IP=$(aws ec2 describe-instances \
-        --instance-ids "$INSTANCE_ID" \
-        --query 'Reservations[0].Instances[0].PublicIpAddress' \
-        --output text)
-
-        RECORD_NAME="$DOMAIN_NAME"
-    else
-        IP=$(aws ec2 describe-instances \
-        --instance-ids "$INSTANCE_ID" \
-        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-        --output text)     
-        RECORD_NAME="$instance.$DOMAIN_NAME"
-    fi   
-
-    echo "Instance for $instance is up and running on IP: $IP"
-
-
-        # ===== CREATE / UPDATE ROUTE53 RECORD =====
     aws route53 change-resource-record-sets \
-        --hosted-zone-id "$ZONE_ID" \
-        --change-batch "{
-            \"Changes\": [{
-                \"Action\": \"UPSERT\",
-                \"ResourceRecordSet\": {
-                    \"Name\": \"$RECORD_NAME\",
-                    \"Type\": \"A\",
-                    \"TTL\": 1,
-                    \"ResourceRecords\": [{\"Value\": \"$IP\"}]
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
                 }
-            }]
-        }"
+                ]
+            }
+            }
+        ]
+    }
+    '
 
-    echo "DNS record created: ${instance}.${DOMAIN_NAME} → $IP"
+    echo "record updated for $instance"
 
 done
