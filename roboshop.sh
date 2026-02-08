b721f7941f7938e628502533a9fc071d0b2f933e
Jai <jaipalreddy1703@gmail.com>
2026-02-08 09:41:16 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -25,7 +25,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
-        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+        RECORD_NAME="$DOMAIN_NAME" # vakiti.online
     else
         IP=$(
             aws ec2 describe-instances \
@@ -33,7 +33,7 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.vakiti.online
     fi
 
     echo "IP Address: $IP"
